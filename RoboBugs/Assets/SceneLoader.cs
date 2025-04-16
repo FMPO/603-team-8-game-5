a5bc048be1cdf10a683c0ee356aec97fc37acf93
FMPO <chrissante3.14@gmail.com>
2025-04-15 21:54:48 -0400

    pushed the menu

---
+++ RoboBugs/Assets/SceneLoader.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GoToGameplayTest()
+    {
+        SceneManager.LoadScene("gameplayTesting");
+    }
+
+    public void GoToGameplay()
+    {
+        SceneManager.LoadScene("GreyboxOne");
+    }
+}
