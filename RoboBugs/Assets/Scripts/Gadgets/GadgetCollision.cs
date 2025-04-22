07177359a3c77857fc671deab1d1bc3338621084
Sfmeyer202 <sfm7027@rit.edu>
2025-04-21 21:44:32 -0400

    Added slime gadget., needs heavy revision.
    - Doesn't like to be placed on corner (Figure out a way to refine placement and scale)
    - Does not properly reduce the velocity.x of the player when interacted with (Not quite sure how to approach)
    - Bugs will occasionally bounce off slime if hit hard enough against it (can be fixed by completely negating values once entered and progressively setting value once inside dependent on placement).

--- RoboBugs/Assets/Scripts/Gadgets/GadgetCollision.cs
+++ RoboBugs/Assets/Scripts/Gadgets/GadgetCollision.cs
@@ -25,6 +25,14 @@ public class NewBehaviourScript : MonoBehaviour
     //A method that detects if something has entered this gameObject's trigger.
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //Left, top, right, bottom.
+        float[] bounds = 
+        {   collision.transform.position.x - collision.transform.localScale.x / 2,
+            collision.transform.position.y + collision.transform.localScale.y / 2,
+            collision.transform.position.x + collision.transform.localScale.x / 2,
+            collision.transform.position.y - collision.transform.localScale.y / 2
+        };
+
         Debug.Log(collision.transform.localScale);
         //If a wall has entered the trigger, this gameObject is replaced with a "placedBumper."
         if (collision.gameObject.tag == "Wall")
@@ -37,7 +45,42 @@ public class NewBehaviourScript : MonoBehaviour
             }
             else
             {
+                Debug.Log("Y :" + bounds[3] + " < " + transform.position.y + " < " + bounds[1]);
+                Debug.Log("X :" + bounds[0] + " < " + transform.position.x + " < " + bounds[2]);
 
+                Vector3 newPosition;
+
+                if (transform.position.x > bounds[0] && transform.position.x < bounds[2])
+                {
+                    //Top Side
+                    if (transform.position.y > collision.transform.position.y)
+                    {
+                        newPosition = new Vector3(transform.position.x, bounds[1], transform.position.z);
+
+                        Instantiate(placedGadget, newPosition, Quaternion.Euler(0, 0, 180));
+                    }
+                    //Bottom Side
+                    else
+                    {
+                        newPosition = new Vector3(transform.position.x, bounds[3], transform.position.z);
+                        Instantiate(placedGadget, newPosition, Quaternion.Euler(0, 0, 0));
+                    }
+                }
+                else if (transform.position.y > bounds[3] && transform.position.y < bounds[1])
+                {
+                    //Left Side
+                    if (transform.position.x < collision.transform.position.x)
+                    {
+                        newPosition = new Vector3(bounds[0], transform.position.y, transform.position.z);
+                        Instantiate(placedGadget, newPosition, Quaternion.Euler(0, 0, 270));
+                    }
+                    //Right Side
+                    else
+                    {
+                        newPosition = new Vector3(bounds[2], transform.position.y, transform.position.z);
+                        Instantiate(placedGadget, newPosition, Quaternion.Euler(0, 0, 90));
+                    }
+                }
             }
         }
     }
