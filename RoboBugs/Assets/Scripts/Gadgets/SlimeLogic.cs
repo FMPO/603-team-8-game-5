79f4c0a63691470ef3d2237e39f91459e8da3854
Sfmeyer202 <sfm7027@rit.edu>
2025-04-23 21:45:47 -0400

    Fixed the vertical slime interaction with the player

--- RoboBugs/Assets/Scripts/Gadgets/SlimeLogic.cs
+++ RoboBugs/Assets/Scripts/Gadgets/SlimeLogic.cs
@@ -1,19 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SlimeLogic : MonoBehaviour
 {
+    private GameObject player;
+    private bool doubleJump;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        doubleJump = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != "Solids")
+        {
+            if (collision.tag == "Player")
+            {
+                collision.GetComponent<Rigidbody2D>().gravityScale = 0;
+                player = collision.gameObject;
+                doubleJump = true;
+                collision.GetComponent<Player>().vspd = 0;
+                collision.GetComponent<Player>().gravity = 0;
+                Debug.Log("pog");
+                //collision.GetComponent<Player>().vspd = collision.GetComponent<Player>().jumpForce;
+                
+                //collision.GetComponent<Player>().hspd *= -transform.up.x;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag != "Solids")
+        {
+            if (collision.tag == "Player")
+            {
+                collision.GetComponent<Player>().gravity = 1;
+                
+                collision.GetComponent<Rigidbody2D>().gravityScale = 1;
+                
+            }
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -28,7 +65,21 @@ public class SlimeLogic : MonoBehaviour
             else
             {
                 collision.GetComponent<Rigidbody2D>().velocity = new Vector2(collision.GetComponent<Rigidbody2D>().velocity.x - collision.GetComponent<Rigidbody2D>().velocity.x * Mathf.Cos(gameObject.transform.rotation.z), collision.GetComponent<Rigidbody2D>().velocity.y - collision.GetComponent<Rigidbody2D>().velocity.y * Mathf.Sin(gameObject.transform.rotation.z));
-                Debug.Log(collision.GetComponent<Rigidbody2D>().velocity);
+                //Debug.Log(collision.GetComponent<Rigidbody2D>().velocity);
+            }
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        
+        //If this is when the context has started, it proceeds.
+        if (context.phase.ToString() == "Started" && Time.timeScale != 0)
+        {
+            if (doubleJump)
+            {
+                doubleJump = false;
+                player.GetComponent<Player>().vspd = player.GetComponent<Player>().jumpForce;
             }
         }
     }
