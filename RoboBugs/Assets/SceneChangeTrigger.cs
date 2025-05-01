597a03f1d811da9e72ae1d6b4f578f0d939c2010
Frank Alfano <q24xboss@gmail.com>
2025-05-01 11:56:19 -0400

    Added door scene change trigger

---
+++ RoboBugs/Assets/SceneChangeTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeTrigger : MonoBehaviour {
+	[SerializeField] private BoxCollider2D triggerCollider;
+	[SerializeField] private int loadSceneBuildIndex;
+
+	private void OnTriggerEnter2D (Collider2D collision) {
+		// If the player collides with the trigger, then load the scene with the specified build index
+		if (collision.CompareTag("Player")) {
+			SceneManager.LoadScene(loadSceneBuildIndex);
+		}
+	}
+}
